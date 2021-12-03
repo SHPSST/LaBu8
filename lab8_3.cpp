917d8e379c3cfcfdc8d3d62326edcd613cca5fb8
Hutao Doctrine <guy_zxc@hotmail.com>
2021-12-03 23:45:42 +0700

    modified

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x>65 && x<91)
+	{
+		return x-1;
+	}
+	else if(x==65)
+	{
+		return 90;
+	}
+	else
+	{
+		return '0';
+	}
 }
 
 int main(){
-	//Test Case
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
